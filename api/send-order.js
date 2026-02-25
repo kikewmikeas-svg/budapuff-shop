06d0d32c4092ed3db5d596dc4e3acfc1a0719b50
kikewmikeas-svg <kikewmikeas@gmail.com>
2026-02-25 22:07:22 +0500

    Update send-order.js

--- api/send-order.js
+++ api/send-order.js
@@ -1,6 +1,6 @@
 const crypto = require("crypto");
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method not allowed" });
   }
