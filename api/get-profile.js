a73391ae90423ffdc2d9f60580d435e0125e7502
kikewmikeas-svg <kikewmikeas@gmail.com>
2026-02-28 21:56:22 +0500

    Update get-profile.js

--- api/get-profile.js
+++ api/get-profile.js
@@ -8,7 +8,7 @@ module.exports = async function handler(req, res) {
     }
 
     const supabaseUrl = process.env.SUPABASE_URL;
-    const supabaseKey = process.env.SUPABASE_ANON_KEY;
+    const supabaseKey = process.env.SUPABASE_SERVICE_KEY;
 
     if (!supabaseUrl || !supabaseKey) {
       return res.status(500).json({ error: "Missing env variables" });
