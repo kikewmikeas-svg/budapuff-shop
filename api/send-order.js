db679d79313596d80e3de0f57b7378c82e188ed1
kikewmikeas-svg <kikewmikeas@gmail.com>
2026-02-28 21:57:23 +0500

    Update send-order.js

--- api/send-order.js
+++ api/send-order.js
@@ -53,7 +53,7 @@ const firstName = userData.first_name || "";
   try {
     // сохраняем заказ в Supabase
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_ANON_KEY;
+const supabaseKey = process.env.SUPABASE_SERVICE_KEY;
     // === Работа с users таблицей ===
 
 // Проверяем есть ли пользователь
