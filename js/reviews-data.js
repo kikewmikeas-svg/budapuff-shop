25bd2ba22dd2e7fa6fba4ec55f691253fd13686e
kikewmikeas-svg <kikewmikeas@gmail.com>
2026-03-07 00:25:29 +0500

    Create reviews-data.js

---
+++ js/reviews-data.js
@@ -0,0 +1,49 @@
+const reviewsData = [
+{
+country:"RU",
+type:"🍄 Грибы",
+product:"Golden Teacher 5г",
+city:"Омск",
+district:"Советский",
+delivery:"Почта (ПВЗ)",
+text:"Квест интересный, но все успешно.",
+rating:"⭐⭐⭐⭐⭐",
+author:"Постоянный клиент"
+},
+
+{
+country:"RU",
+type:"🌿 Чай",
+product:"Раслабляющий чай Кратом 250г",
+city:"Москва",
+district:"ЮАО",
+delivery:"Постамат",
+text:"Все четко забрал, чай понравился.",
+rating:"⭐⭐⭐⭐⭐",
+author:"Анонимный покупатель"
+},
+
+{
+country:"KZ",
+type:"💨 Вейп",
+product:"9 Pound Hammer | 3 мл x2",
+city:"Астана",
+district:"Алматинский",
+delivery:"Почта (ПВЗ)",
+text:"Все пришло быстро, вейп топ.",
+rating:"⭐⭐⭐⭐⭐",
+author:"Анонимный покупатель"
+},
+
+{
+country:"RB",
+type:"🧃 Жидкость",
+product:"Maui Wowie | 30 мл",
+city:"Минск",
+district:"Центральный",
+delivery:"СДЭК ПВЗ",
+text:"Жидкость отличная, вкус зашел.",
+rating:"⭐⭐⭐⭐⭐",
+author:"Анонимный покупатель"
+}
+];
