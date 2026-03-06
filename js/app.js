27028cbd51a9c2e833321fec6340a4e405f55dd2
kikewmikeas-svg <kikewmikeas@gmail.com>
2026-03-06 20:06:00 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 console.log("app.js loaded");
+
+const tg = window.Telegram.WebApp;
+const backButton = tg.BackButton;
+
+if (tg.initData) {
+  fetch("/api/log-visit", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ initData: tg.initData })
+  });
+}
