4d47b725370f7ec0af8b98939d87cde591873afd
kikewmikeas-svg <kikewmikeas@gmail.com>
2026-03-06 18:23:58 +0500

    Create crypto-products.js

---
+++ js/crypto-products.js
@@ -0,0 +1,23 @@
+export const cryptoProducts = [
+
+{
+id: "pine",
+name: "Шишки кедровые",
+weights: {
+"1g": 1200,
+"2g": 2200,
+"5g": 5000
+}
+},
+
+{
+id: "hematogen",
+name: "Гематоген",
+weights: {
+"1g": 900,
+"2g": 1700,
+"5g": 3900
+}
+}
+
+]
