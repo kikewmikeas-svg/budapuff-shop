d1bbcd09ae7d7da68cdcec82977af76b7deecfb0
kikewmikeas-svg <kikewmikeas@gmail.com>
2026-03-06 19:13:18 +0500

    Update crypto-products.js

--- js/crypto-products.js
+++ js/crypto-products.js
@@ -1,23 +1,49 @@
-export const cryptoProducts = [
+export const cryptoProducts = {
 
+"🌲 Кедровые шишки":[
 {
-id: "pine",
-name: "Шишки кедровые",
-weights: {
-"1g": 1200,
-"2g": 2200,
-"5g": 5000
-}
+name:"Classic",
+price:1500
 },
-
 {
-id: "hematogen",
-name: "Гематоген",
-weights: {
-"1g": 900,
-"2g": 1700,
-"5g": 3900
+name:"Forest",
+price:1700
+},
+{
+name:"Premium",
+price:2000
+},
+{
+name:"Gold",
+price:2300
+},
+{
+name:"Elite",
+price:2600
 }
-}
+],
 
+"🍫 Гематоген":[
+{
+name:"Classic",
+price:500
+},
+{
+name:"Milk",
+price:550
+},
+{
+name:"Strong",
+price:600
+},
+{
+name:"Energy",
+price:650
+},
+{
+name:"Premium",
+price:700
+}
 ]
+
+}
