5fe3cf708393877055a87bb9aa1212268cafecd6
kikewmikeas-svg <kikewmikeas@gmail.com>
2026-03-09 20:09:11 +0500

    Update new-app.js

--- new-shop/new-app.js
+++ new-shop/new-app.js
@@ -1,1 +1,26 @@
+function enterNewShop(){
 
+document.getElementById("main").innerHTML = `
+
+<div class="new-shop">
+
+<button onclick="leaveNewShop()">← Вернуться в старый магазин</button>
+
+<h2>Новый магазин</h2>
+
+<div id="new-shop-content"></div>
+
+</div>
+
+`;
+
+}
+function leaveNewShop(){
+
+if(confirm("Вы уверены? Все данные нового магазина будут очищены.")){
+
+renderCategories();
+
+}
+
+}
