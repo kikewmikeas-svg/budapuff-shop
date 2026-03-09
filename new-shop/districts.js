15e370eaa989a0830cc50f243ab8f9bc5e59174b
kikewmikeas-svg <kikewmikeas@gmail.com>
2026-03-09 23:06:28 +0500

    Create districts.js

---
+++ new-shop/districts.js
@@ -0,0 +1,33 @@
+const districtsDB = {
+
+"Москва": [
+
+{ name:"ЮЗАО", type:"magnet", available:true },
+{ name:"ТиНАО", type:"magnet", available:true },
+{ name:"Зеленоградский", type:"magnet", available:true },
+
+{ name:"ЮВАО", type:"lock", available:false },
+{ name:"СЗАО", type:"lock", available:false },
+{ name:"СВАО", type:"lock", available:false },
+
+{ name:"САО", type:"lock", available:false },
+{ name:"ЦАО", type:"lock", available:false },
+{ name:"ЗАО", type:"lock", available:false },
+
+{ name:"ЮАО", type:"lock", available:false },
+{ name:"ВАО", type:"lock", available:false }
+
+],
+
+"Омск":[
+
+{ name:"Центральный", type:"magnet", available:true },
+{ name:"Кировский", type:"magnet", available:true },
+{ name:"Ленинский", type:"magnet", available:true },
+
+{ name:"Советский", type:"lock", available:false },
+{ name:"Октябрьский", type:"lock", available:false }
+
+]
+
+};
