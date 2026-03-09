ed5bdd7ced749cbf1f46a436a1983af3b6191795
kikewmikeas-svg <kikewmikeas@gmail.com>
2026-03-09 22:34:43 +0500

    Create new-city.js

---
+++ new-shop/new-city.js
@@ -0,0 +1,54 @@
+const CITY_KEY = "newShopCity";
+
+function getSavedCity(){
+return localStorage.getItem(CITY_KEY);
+}
+
+function saveCity(city){
+localStorage.setItem(CITY_KEY, city);
+}
+
+function showCitySelect(){
+
+document.getElementById("main").innerHTML = `
+
+<div class="city-screen">
+
+<h1>Выберите город</h1>
+
+<input 
+id="cityInput"
+type="text"
+placeholder="Введите город"
+autocomplete="off"
+/>
+
+<div id="citySuggestions"></div>
+
+<button onclick="confirmCity()">Войти</button>
+
+</div>
+
+`;
+
+initCitySearch();
+
+}
+
+function confirmCity(){
+
+const input = document.getElementById("cityInput");
+const city = input.value.trim();
+
+if(!city){
+alert("Введите город");
+return;
+}
+
+saveCity(city);
+
+enterNewShop();
+
+}
+
+window.showCitySelect = showCitySelect;
