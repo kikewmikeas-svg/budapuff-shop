cdd9a331083fb732b5d9f84cd9ae49ac45dc5733
kikewmikeas-svg <kikewmikeas@gmail.com>
2026-03-09 18:21:23 +0500

    Update new-app.js

--- new-section/new-app.js
+++ new-section/new-app.js
@@ -28,3 +28,35 @@ html += `</div>`;
 document.getElementById("main").innerHTML = html;
 
 }
+function openNewCategory(category){
+
+const subs = newProducts[category];
+
+let html = `
+<button id="backBtn" onclick="renderNewSection()">← Назад</button>
+
+<h2>${category}</h2>
+
+<div class="products-grid">
+`;
+
+Object.keys(subs).forEach(sub => {
+
+html += `
+<div class="product-card"
+onclick="openNewSub('${category}','${sub}')">
+
+<div class="category-content">
+<div class="category-title">${sub}</div>
+</div>
+
+</div>
+`;
+
+});
+
+html += `</div>`;
+
+document.getElementById("main").innerHTML = html;
+
+}
